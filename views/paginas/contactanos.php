de87c72fd3b8d7b25e9d273736ca770002981494
ivette03 <iacascante@est.istg.edu.ec>
2024-04-30 01:24:04 -0500

    creating my contact form

--- views/paginas/contactanos.php
+++ views/paginas/contactanos.php
@@ -1,10 +1,46 @@
-<main>
+<main class="flex justify-center items-center flex-col space-y-4">
     <div>
-        <h2>Formulario de contacto</h2>
+        <h2 class="text-2xl font-bold">Formulario de contacto</h2>
     </div>
-    <form>
-        
+    <form class="max-w-md w-full">
+        <fieldset class="border border-gray-300 p-4 rounded-md">
+            <legend class="text-lg font-bold">Información Personal</legend>
+            <label for="nombre" class="block mt-2">Nombre:</label>
+            <input placeholder="Escriba su nombre" type="text" name="nombre" id="nombre" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+            <label for="apellido" class="block mt-2">Apellido:</label>
+            <input placeholder="Escriba su apellido" type="text" name="apellido" id="apellido" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+            <label for="telefono" class="block mt-2">Teléfono:</label>
+            <input placeholder="Escriba su Teléfono" type="text" name="telefono" id="telefono" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+            <label for="mensaje" class="block mt-2">Mensaje:</label>
+            <textarea placeholder="Escriba un mensaje"  name="mensaje" id="mensaje" class="w-full border border-gray-300 rounded-md p-2 mt-1"></textarea>
+        </fieldset>
+
+        <fieldset class="border border-gray-300 p-4 rounded-md">
+            <legend class="text-lg font-bold">Información sobre un curso</legend>
+            <select id="opciones" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+                <option value="" disabled selected>--Seleccione--</option>
+                <option value="canto">Canto</option>
+                <option value="piano">Piano</option>
+                <option value="bateria">Bateria</option>
+            </select>
+        </fieldset>
+
+
+        <fieldset class="border border-gray-300 p-4 rounded-md">
+            <legend class="text-lg font-bold">Información de medio de contacto</legend>
+            <p class="mt-2">Como desea ser contactado?</p>
+            <div class="flex items-center mt-1">
+                <label for="contactar-telefono" class="mx-4">Teléfono</label>
+                <input name="contacto" type="radio" value="telefono" id="contactar-telefono" class="form-radio">
+                <label for="contactar-email" class="mx-4">E-mail</label>
+                <input name="contacto" type="radio" value="email" id="contactar-email" class="form-radio">
+            </div>
+            <p class="mt-2">Si eligió teléfono, elija la fecha y la hora</p>
+            <label for="fecha" class="block mt-2">Fecha:</label>
+            <input type="date" id="fecha" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+            <label for="hora" class="block mt-2">Hora:</label>
+            <input type="time" id="hora" min="09:00" max="18:00" class="w-full border border-gray-300 rounded-md p-2 mt-1">
+        </fieldset>
+
     </form>
-
-
 </main>
